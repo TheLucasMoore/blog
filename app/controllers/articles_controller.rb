026aea9fe1c11d6c8e73e8dca4bf9501b4c6be32
thelucasmoore <lucasedwardmoore@gmail.com>
2015-10-17 13:30:09 -0400

    Error of 0 Arguments?

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,25 @@
 class ArticlesController < ApplicationController
+
+	def show
+		@article = Article.find{params [:id]}
+	end
+
 	def new
 	end
 
+	def index
+	end
+
 	def create
-		@article = Article.new(params.require{:article}.permit(:title, :text))
+		@article = Article.new{article_params}
 
 		@article.save
 		redirect_to @article
 	end
+
+	private
+		def article_params
+			params.require(:article).permit(:title, :text)
+		end
 
 end
